861b1dd190b8d800b142247b789a9f726d5eec07
FelipeMoraUAO <andres_f.mora@uao.edu.co>
2025-09-04 23:13:59 -0500

    Documentacion

--- Taller_1/Assets/Scripts/Cajero.cs
+++ Taller_1/Assets/Scripts/Cajero.cs
@@ -3,11 +3,11 @@ using UnityEngine;
 public class Cajero : MonoBehaviour
 {
     string nombre;
-    ColaManager manager;
+    ColaManager manager; //Trae los lcientes de la cola
     float tiempoRestante = 0;
-    Cliente clienteActual = null;
+    Cliente clienteActual = null; //Identifica si esta atendiendo a algun cliente
 
-    public void Init(ColaManager m, string n)
+    public void Init(ColaManager m, string n) //Llama con el init a el script ColaManager y lo poner a controlar al cajero y luego le asigna un nombre
     {
         manager = m;
         nombre = n;
--- Taller_1/Assets/Scripts/ColaManager.cs
+++ Taller_1/Assets/Scripts/ColaManager.cs
@@ -4,22 +4,22 @@ using UnityEngine.UI;
 
 public class ColaManager : MonoBehaviour
 {
-    public Cajero[] cajeros;   // Arrastrar en el Inspector
-    public Button BtnIniciar;  // Botón iniciar
-    public Button BtnDetener;  // Botón detener
+    public Cajero[] cajeros;
+    public Button BtnIniciar;  // Botón de iniciar
+    public Button BtnDetener;  // Botón de detener
 
-    Queue<Cliente> cola = new Queue<Cliente>();
-    bool generando = false;
-    int consecutivo = 1;
+    Queue<Cliente> cola = new Queue<Cliente>(); //Crea la cola
+    bool generando = false; //Genera los clientes (inicia en false para poder dar inicio con los btones
+    int consecutivo = 1; //Contador de clientes en la cola
 
     void Start()
     {
-        // Conectar botones
-        BtnIniciar.onClick.AddListener(Iniciar);
-        BtnDetener.onClick.AddListener(Detener);
+        // Conectar botones con unity
+        BtnIniciar.onClick.AddListener(Iniciar); //onClick (cuando haga click) y addLis... (ejecutar metodo x)
+        BtnDetener.onClick.AddListener(Detener);//onClick (cuando haga click) y addLis... (ejecutar metodo x)
 
         // Dar nombres a los cajeros
-        for (int i = 0; i < cajeros.Length; i++)
+        for (int i = 0; i < cajeros.Length; i++) //Recorre a todos los cajeros. El Length es la cantidad total del arreglo
         {
             cajeros[i].Init(this, "Cajero " + (i + 1));
         }
@@ -28,16 +28,16 @@ public class ColaManager : MonoBehaviour
     void Update()
     {
         // Si está activado, generar clientes cada cierto tiempo
-        if (generando && Time.frameCount % 120 == 0) // cada ~2 segundos
+        if (generando && Time.frameCount % 120 == 0) // cada ~2 segundos (120 frames = 2 seg si el juego esta a 60 frames/seg
         {
-            string id = "C" + consecutivo++;
-            Tramite tr = (Random.value < 0.5f) ? Tramite.Retirar : Tramite.Consignar;
-            float tAt = Random.Range(2f, 5f);
+            string id = "Cliente" + consecutivo++; //Crea un nombre para cada cliente "Cliente 1" etc
+            Tramite tr = (Random.value < 0.5f) ? Tramite.Retirar : Tramite.Consignar; // Genera valor aleatorio ente 0 y 1 (x<0,5 retira y x>0,5 consigna
+            float tAt = Random.Range(2f, 5f); //Crea un valor que determina el tiempo de atencion al cliente entre 2 a 5 seg
 
-            Cliente c = new Cliente(id, "Cliente " + id, id + "@mail.com", "Calle X", tr, tAt);
-            cola.Enqueue(c);
+            Cliente c = new Cliente(id, "Cliente " + id, id + "@mail.com", "Calle X", tr, tAt); //Se instacion nuevo cliente con sus datos
+            cola.Enqueue(c); //Se mete el cliente a la cola
 
-            Debug.Log("➡ Entra " + c.idCliente + " - " + tr + " - " + tAt.ToString("0.0") + "s");
+            Debug.Log("Entra " + c.idCliente + " - " + tr + " - " + tAt.ToString("0.0") + "s"); //Mensaje de consola
         }
     }
 
